69d4c74eba2de3c7eabc816541eee750faa9a4d6
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-15 17:43:15 -0400

    adding weather and movies components

--- data/modules/weather.js
+++ data/modules/weather.js
@@ -1,1 +1,27 @@
-// const axios = require('axios');
+const axios = require('axios');
+
+async function getWeather(request, response, next) {
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+    let weatherDataFromAxios = await axios.get(weatherURL);
+    console.log(weatherDataFromAxios.data);
+    let weatherData = weatherDataFromAxios.data.data.map(cityObj => new Forecast(cityObj));
+
+    response.status(200).send(weatherData);
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+class Forecast {
+  constructor(cityObj) {
+    this.date = cityObj.datetime;
+    this.description = cityObj.weather.description;
+  }
+}
+
+
+module.exports = getWeather;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const cors = require('cors');
 const axios = require('axios');
 const getMovies = require('./data/modules/movies');
+const getWeather = require('./data/modules/weather');
 
 // DATA JSON TO USE
 // let data = require('./data/weather.json');
@@ -20,33 +21,9 @@ const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`We are running on ${PORT}!`));
 
 
-///movies?searchQuery=seattle
 app.get('/movies', getMovies);
 
-
-class Forecast {
-  constructor(cityObj) {
-    this.date = cityObj.datetime;
-    this.description = cityObj.weather.description;
-  }
-}
-
-// http://localhost/weather?lat=47.6062&lon=-122.3321
-app.get('/weather', async (request, response, next) => {
-
-  try {
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-    let weatherDataFromAxios = await axios.get(weatherURL);
-    console.log(weatherDataFromAxios.data);
-    let weatherData = weatherDataFromAxios.data.data.map(cityObj => new Forecast(cityObj));
-
-    response.status(200).send(weatherData);
-  } catch (error) {
-    next(error);
-  }
-});
+app.get('/weather', getWeather);
 
 
 app.get('*', (request, response) => {
